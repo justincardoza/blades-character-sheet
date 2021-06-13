429debf7a7ef1457fa019f9a7e8633dcd5383aa3
Justin Cardoza <1139831+justincardoza@users.noreply.github.com>
2021-06-13 16:55:06 -0700

    Started page structure, added multi-character support.

--- main.js
+++ main.js
@@ -2,6 +2,37 @@ window.addEventListener('DOMContentLoaded', function()
 {
 	var app = new Vue({
 		el: '#app',
-		data: { characterName: 'Baszo Baz' },
+		data: {
+			currentCharacter: 0,
+			
+			characters: [
+				{ 
+					fullName: 'Baszo Baz', 
+					alias: '', 
+					look: '', 
+					heritageType: '',
+					heritageDescription: '',
+					backgroundType: '', 
+					backgroundDescription: '',
+					viceType: '', 
+					viceDescription: '',
+					stress: 0,
+					traumas: [],
+					harms: [],
+					healingClock: 0,
+					usesArmor: false,
+					usesHeavyArmor: false,
+					usesSpecialArmor: false,
+					xpChallengeType: '',
+					friends: [],
+					enemies: [],
+					specialAbilities: [],
+					specialAbilityXP: 0,
+					coin: 0,
+					stash: [0, 0, 0, 0],
+					
+				}
+			]
+		},
 	});
 });
